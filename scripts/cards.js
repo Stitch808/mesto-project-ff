7433f996b4fb79d4abbb78c89528d9a0dc8adeda
Stitch <kondratyuk_serezhenka@inbox.ru>
2024-02-12 16:06:56 +0300

    5_sprint

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,27 @@
 const initialCards = [
-    {
+    cardData = {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     },
-    {
+    cardData = {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
     },
-    {
+    cardData = {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
     },
-    {
+    cardData = {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
     },
-    {
+    cardData = {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
     },
-    {
+    cardData = {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+
+function getCard(iteam, deletCard) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+	const cardImage = cardElement.querySelector('.card__image');
+	const cardTitle = cardElement.querySelector('.card__title');
+	cardImage.src = iteam.link;
+	cardImage.alt = iteam.name;
+	cardTitle.textContent = iteam.name;
+  
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+	deleteButton.addEventListener('click', function() {
+    deletCard(cardElement)
+  });
+
+	return cardElement;
+
+}
 
 // @todo: Функция создания карточки
+function addCard(cardData) {
+	cardList.append(getCard(cardData, deletCard))
+}
 
 // @todo: Функция удаления карточки
+function deletCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((function(element) {
+	addCard(element)
+}))
